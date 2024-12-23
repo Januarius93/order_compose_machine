7d1ba5a80a3389dcd6373df64aa17749d9b210ef
Januarius93 <marstyczen@gmail.com>
2024-12-23 14:10:03 +0100

    init commit

---
+++ src/main/java/org/restaurant/order_compose_machine/OrderComposeMachineApplication.java
@@ -0,0 +1,14 @@
+package org.restaurant.order_compose_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderComposeMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderComposeMachineApplication.class, args);
+	}
+
+}
